300e2fad30a0173b6115d242ec4008fac862dab4
kyle.leitham <kyle.leitham@gmail.com>
2020-09-24 16:23:48 -0400

    Initial React web app commit.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <p>This is Kyle Leitham's personal website.</p>
+    </div>
+  );
+}
+
+export default App;
