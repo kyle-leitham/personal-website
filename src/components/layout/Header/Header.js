0584836d03fb5dfcf9a863ddca251f7f65f4d0aa
kyle.leitham <kyle.leitham@gmail.com>
2020-09-24 20:33:36 -0400

    Added space in json icon

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -14,7 +14,7 @@ function Header() {
                     <li><span className="active"><i className="fa fa-html5"></i><a className="active" href="#">Home.html</a></span></li>
                     <li><span><i className="fa fa-css3"></i><a href="#">About.css</a></span></li>
                     <li><span><i className="fab fa-js"></i><a href="#">Portfolio.js</a></span></li>
-                    <li><span><label>{'{'}{'}'}</label><a href="#">ContactMe.json</a></span></li>
+                    <li><span><label>{'{'} {'}'}</label><a href="#">ContactMe.json</a></span></li>
                 </ul>
             </nav>
         </header>
