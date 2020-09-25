b2fe9e37fb77c80b40213adbae5c07f9b686eca6
kyle.leitham <kyle.leitham@gmail.com>
2020-09-24 23:14:54 -0400

    Added home page greeting

--- src/components/layout/MainContent/MainContent.js
+++ src/components/layout/MainContent/MainContent.js
@@ -4,7 +4,19 @@ import './MainContent.scss';
 
 function MainContent() {
     return (
-        <main></main>
+        <main>
+            <div className="greeting">
+                <p className="tag">{'<p>'}</p>
+                <p className="innerText">Hello World!</p>
+                <p className="tag">{'</p>'}</p>
+            </div>
+            <div className="name">
+                <h1 className="tag">{'<h1>'}</h1>
+                <h1 className="innerText">I'm </h1>
+                <h1 className="innerTextBold">Kyle Leitham.</h1>
+                <h1 className="tag">{'</h1>'}</h1>
+            </div>
+        </main>
     );
 }
 
